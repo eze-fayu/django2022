a6d45a486a44b10f8d3687da344f6e412583b133
Fayu <r826720@hotmail.com>
2022-10-30 18:23:36 -0300

    cambiado colores bootstrap. +contacto. +fin_navbar

--- G13/main/views.py
+++ G13/main/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render, redirect
+from .forms import ContactoForm
 
 # Create your views here.
 
 def index(request):
     return render(request, "main/index.html")
+
+def contacto(request):
+    if request.method == "POST":
+        contacto_form = ContactoForm(request.POST)
+        contacto_form.is_valid()
+        
+        
+    else:
+        contacto_form = ContactoForm()
+    return render(request, "contacto/contacto.html", {'contacto_form': contacto_form})
+

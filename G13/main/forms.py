a4a97ec984613786f4be50b0e7d3e689e709319b
Fayu <r826720@hotmail.com>
2022-10-30 21:50:21 -0300

    cambio x textarea

--- G13/main/forms.py
+++ G13/main/forms.py
@@ -5,4 +5,4 @@ class ContactoForm(forms.Form):
     nombre = forms.CharField(label="Contacto:", required=False, max_length=10)
     apellido = forms.CharField(label="Apellido de contacto", required=False)
     email = forms.EmailField()
-    mensaje = forms.CharField(label="Mensaje", required=True)
+    mensaje = forms.CharField(widget=forms.Textarea(attrs={"rows":"5", "label":"Apellido de contacto", "required":"False"}))

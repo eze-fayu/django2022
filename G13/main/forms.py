5d953ebe63751c6c7e20b7c4b28516d321c05e34
Ezequiel <ezequielfaiella@gmail.com>
2022-10-31 19:36:51 -0300

    empiezo en el envio del formulario por mail

--- G13/main/forms.py
+++ G13/main/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class ContactoForm(forms.Form):
-    nombre = forms.CharField(label="Contacto:", required=False, max_length=10)
-    apellido = forms.CharField(label="Apellido de contacto", required=False)
+    nombre = forms.CharField(label="Nombre:", required=False, max_length=50)
+    apellido = forms.CharField(label="Apellido de contacto", required=False, max_length=50)
     email = forms.EmailField()
-    mensaje = forms.CharField(widget=forms.Textarea(attrs={"rows":"5", "label":"Apellido de contacto", "required":"False"}))
+    mensaje = forms.CharField(widget=forms.Textarea(attrs={"rows":"5", "label":"Apellido de contacto", "required":"True"}))
--- G13/main/views.py
+++ G13/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import ContactoForm
+# from django.core.mail import EmailMessage
+# from django.template.loader import render_to_string
 
 # Create your views here.
 
@@ -9,8 +11,13 @@ def index(request):
 def contacto(request):
     if request.method == "POST":
         contacto_form = ContactoForm(request.POST)
-        contacto_form.is_valid()
-        
+        if contacto_form.is_valid():
+            # request.post es un diccionario que tiene :
+            # <QueryDict: {'csrfmiddlewaretoken': ['8359bj78AaLzCqUP7Ubk541YhDteAlIG87S6WnamkkzbJA9PK39gVid3sl8pxPSX'], 'nombre': ['casillacon'], 'apellido': ['casilape'], 'email': ['main@mail.com'], 'mensaje': ['mensaje_enviado']}>
+            nombre = contacto_form.cleaned_data['nombre']
+            apellido = contacto_form.cleaned_data['apellido']
+            email = contacto_form.cleaned_data['email']
+            mensaje = contacto_form.cleaned_data['mensaje']
         
     else:
         contacto_form = ContactoForm()

bd4da31c7055a8b3e92da6b7988a3c20e2e9ac3e
Fayu <r826720@hotmail.com>
2022-12-10 09:58:28 -0300

    desactivado staticfiles

--- G13/G13/settings.py
+++ G13/G13/settings.py
@@ -137,9 +137,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 

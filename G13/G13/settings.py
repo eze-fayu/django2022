9cd5f5d623adff04e801332bce426a6db2d006a4
Fayu <r826720@hotmail.com>
2022-12-10 15:15:10 -0300

    cambio static root

--- G13/G13/settings.py
+++ G13/G13/settings.py
@@ -141,7 +141,7 @@ STATIC_URL = '/static/'
 #     BASE_DIR / 'static'
 # ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'static_root'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
